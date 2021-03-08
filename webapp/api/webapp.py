0ae3f9beb0fc0406356bc6f3539cd2611f3b9f5e
alirionx <dquilitzsch@outlook.de>
2021-03-08 01:13:28 +0000

    UIUIUIUI

--- webapp/api/webapp.py
+++ webapp/api/webapp.py
@@ -73,6 +73,53 @@ def api_mashes_get():
   return jsonify(resObj), 200
 
 #------------------------------------------
+@app.route('/api/rate/<mashId>', methods=['GET'])
+def api_pics_rate_get(mashId):
+  status = 200
+  resObj = {
+    "path": "/api/rate/%s" %mashId,
+    "method": "GET",
+    "msg": "",
+  }
+ 
+  myPics = pics()
+  res = myPics.random_mash(mashId)
+
+  if not res:
+    status = 500
+    resObj["msg"] = "mash does not exists or not enought pictures..." 
+    resObj["status"] = status
+    return jsonify(resObj), status
+  
+  resObj["data"] = res
+  resObj["status"] = status
+  return jsonify(resObj), status
+
+#------------------------------------------
+@app.route('/api/rate', methods=['POST'])
+def api_rate():
+  status = 200
+  resObj = {
+    "path": "/api/rate/%s",
+    "method": "GET",
+    "msg": "",
+  }
+  
+  rateIn = request.get_json()
+  print(rateIn)
+  myPics = pics()
+  res = myPics.rate_mash(rateIn)
+
+  if not res:
+    status = 500
+    resObj["msg"] = "Failed to rate mash.." 
+    resObj["status"] = status
+    return jsonify(resObj), status
+  
+  resObj["status"] = status
+  return jsonify(resObj), status
+
+#------------------------------------------
 @app.route('/api/mash', methods=['PUT'])
 def api_mashes_change():
   status = 200
@@ -148,7 +195,12 @@ def api_mashes_delete(curId):
   }
 
   myDbTool = db_tool()
-  sqlRes = myDbTool.execute_delete("DELETE FROM pics WHERE mash = %s ;" %int(curId) )
+  myPics = pics()
+  #sqlRes = myDbTool.execute_delete("DELETE FROM pics WHERE mash = %s ;" %int(curId) )
+  sqlRes = myDbTool.execute_select("SELECT id FROM pics WHERE mash = %s ;" %int(curId) )
+  for row in sqlRes:
+    picId = row["id"]
+    myPics.remove_pic(picId)
 
   sqlRes = myDbTool.execute_delete("DELETE FROM mashes WHERE id = %s ;" %int(curId) )
   if sqlRes == 0:
@@ -171,25 +223,36 @@ def api_pics_get(mashId):
     "msg": "",
   }
 
-  picAry = []
   myDbTool = db_tool()
+  sqlRes = myDbTool.execute_select("SELECT * FROM mashes WHERE id = %s;" %int(mashId) )
+  if len(sqlRes) == 0:
+    status = 404
+    resObj["msg"] = "mash not found" 
+    resObj["status"] = status
+    return jsonify(resObj), status
+  
+  resObj["mash"] = sqlRes[0]
+
+  picAry = []
   sqlRes = myDbTool.execute_select("SELECT * FROM pics WHERE mash = %s;" %int(mashId) )
   for row in sqlRes:
     won = int(row["won"])
     loss = int(row["loss"])
     if won == 0 and loss == 0:
-      rate = '50%'
+      rate = 50
     elif won == 0:
-      rate = '0%'
+      rate = 0
     elif loss == 0:
-      rate = '100%'
+      rate = 100
     else:
-      rate = won/(won+loss)
+      rate = round( won/(won+loss)*100 ) 
     
-    row["rate"] = rate
+    row["rate"] = str(rate) + '%'
+    row["rateNmb"] = rate
     picAry.append(row)
 
-  resObj["data"] = picAry
+  srtPicAry = sorted(picAry, key=lambda k: k['rateNmb'], reverse=True) #UIUIUIUIUIUIUIUIUI
+  resObj["data"] = srtPicAry
   
   if "admin" in session:
      resObj["admin"] = True
@@ -228,6 +291,28 @@ def api_pics_upload(mashId):
   resObj["status"] = status
   return jsonify(resObj), status
 
+
+#----------------------------------------------
+@app.route('/api/pic/<picId>', methods=['DELETE'])
+def api_pic_delete(picId):
+  status = 200
+  resObj = {
+    "path": "/api/pic/%s" %picId,
+    "method": "DELETE",
+    "msg": "",
+  }
+
+  myPics = pics()
+
+  if not myPics.remove_pic(picId):
+    status = 404
+    resObj["msg"] = "pic not found" 
+    resObj["status"] = status
+    return jsonify(resObj), status
+
+
+  resObj["status"] = status
+  return jsonify(resObj), status
 
 #----------------------------------------------
 
